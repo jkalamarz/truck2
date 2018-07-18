f0ed99bda049a88d59e9d14db4648acba7521163
jkalamarz <jkalamarz@gmail.com>
2018-07-18 16:21:49 -0700

    Minor update

---
+++ Assets/Scripts/WheelDrive.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public enum DriveType
+{
+	RearWheelDrive,
+	FrontWheelDrive,
+	AllWheelDrive
+}
+
+public class WheelDrive : MonoBehaviour
+{
+    [Tooltip("Maximum steering angle of the wheels")]
+	public float maxAngle = 30f;
+	[Tooltip("Maximum torque applied to the driving wheels")]
+	public float maxTorque = 300f;
+	[Tooltip("Maximum brake torque applied to the driving wheels")]
+	public float brakeTorque = 30000f;
+	[Tooltip("If you need the visual wheels to be attached automatically, drag the wheel shape here.")]
+	public GameObject wheelShape;
+
+	[Tooltip("The vehicle's speed when the physics engine can use different amount of sub-steps (in m/s).")]
+	public float criticalSpeed = 5f;
+	[Tooltip("Simulation sub-steps when the speed is above critical.")]
+	public int stepsBelow = 5;
+	[Tooltip("Simulation sub-steps when the speed is below critical.")]
+	public int stepsAbove = 1;
+
+	[Tooltip("The vehicle's drive type: rear-wheels drive, front-wheels drive or all-wheels drive.")]
+	public DriveType driveType;
+
+    private WheelCollider[] m_Wheels;
+
+
+	public AudioSource m_MovementAudio;    
+    public AudioClip m_EngineIdling;       
+    public AudioClip m_EngineDriving;      
+    public float m_PitchRange = 0.2f;
+	public float m_OriginalPitch;
+
+    // Find all the WheelColliders down in the hierarchy.
+	void Start()
+	{
+		m_Wheels = GetComponentsInChildren<WheelCollider>();
+
+		for (int i = 0; i < m_Wheels.Length; ++i) 
+		{
+			var wheel = m_Wheels [i];
+
+			// Create wheel shapes only when needed.
+			if (wheelShape != null)
+			{
+				var ws = Instantiate (wheelShape);
+				ws.transform.parent = wheel.transform;
+			}
+		}
+		m_OriginalPitch = m_MovementAudio.pitch;
+	}
+
+	private void EngineAudio(float torque)
+    {
+		var body = GetComponent<Rigidbody>();
+        // Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
+
+        if (Mathf.Abs(torque) > 0.1 || body.velocity.magnitude > 10f)
+        {
+            if (m_MovementAudio.clip == m_EngineIdling)
+            {
+                m_MovementAudio.clip = m_EngineDriving;
+                m_MovementAudio.Play();
+            }
+            m_MovementAudio.pitch = m_OriginalPitch * (10f + body.velocity.magnitude % 10f + body.velocity.magnitude) / 10f;
+        }
+        else
+        {
+            if (m_MovementAudio.clip == m_EngineDriving)
+            {
+                m_MovementAudio.clip = m_EngineIdling;
+                m_MovementAudio.pitch = UnityEngine.Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+        }
+    }
+
+	// This is a really simple approach to updating wheels.
+	// We simulate a rear wheel drive car and assume that the car is perfectly symmetric at local zero.
+	// This helps us to figure our which wheels are front ones and which are rear.
+	void Update()
+	{
+		m_Wheels[0].ConfigureVehicleSubsteps(criticalSpeed, stepsBelow, stepsAbove);
+
+		float angle = maxAngle * Input.GetAxis("Horizontal");
+		float torque = maxTorque * Input.GetAxis("Vertical");
+
+		float handBrake = Input.GetKey(KeyCode.Space) ? brakeTorque : 0;
+
+		foreach (WheelCollider wheel in m_Wheels)
+		{
+			// A simple car where front wheels steer while rear ones drive.
+			if (wheel.name.StartsWith("f"))
+				wheel.steerAngle = angle;
+
+    		wheel.brakeTorque = handBrake;
+
+			if (wheel.name.StartsWith("r") && driveType != DriveType.FrontWheelDrive)
+			{
+				wheel.motorTorque = torque;
+			}
+
+			if (wheel.name.StartsWith("f") && driveType != DriveType.RearWheelDrive)
+			{
+				wheel.motorTorque = torque;
+			}
+
+			// Update visual wheels if any.
+			if (wheelShape) 
+			{
+				Quaternion q;
+				Vector3 p;
+				wheel.GetWorldPose (out p, out q);
+
+				// Assume that the only child of the wheelcollider is the wheel shape.
+				Transform shapeTransform = wheel.transform.GetChild (0);
+				shapeTransform.position = p;
+				shapeTransform.rotation = q;
+			}
+		}
+
+		EngineAudio(torque);
+	}
+}
---
+++ Assets/SimpleCarController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class AxleInfo {
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+}
+     
+public class SimpleCarController : MonoBehaviour {
+    public List<AxleInfo> axleInfos; 
+     
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider, float steering)
+    {
+        if (collider.transform.childCount == 0) {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+     
+        Vector3 position;
+        Quaternion r;
+        collider.GetWorldPose(out position, out r);
+     
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = r;
+            //Quaternion.AngleAxis(steering * Input.GetAxis("Horizontal"), Vector3.forward);
+    }
+     
+    public void FixedUpdate()
+    {
+        foreach (AxleInfo axleInfo in axleInfos) {
+            float motor = axleInfo.maxMotorTorque * Input.GetAxis("Vertical");
+            float steering = axleInfo.maxSteeringAngle * Input.GetAxis("Horizontal");
+            
+            axleInfo.leftWheel.steerAngle = steering;
+            axleInfo.rightWheel.steerAngle = steering;
+            
+            axleInfo.leftWheel.motorTorque = motor;
+            axleInfo.rightWheel.motorTorque = motor;
+
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel, steering);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel, steering);
+        }
+    }
+}
